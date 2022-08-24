e55005ed7d0740d7a1debeed7ed9f8dc2d9138da
Alvin Varghese <alvin19official@gmail.com>
2022-08-24 15:36:11 +0530

    feat: added logic

--- script.js
+++ script.js
@@ -1,24 +1,15 @@
-//VARIABLES
-let data = {
-    operand1: undefined,
-    operation1: undefined,
-    operand2: undefined
-};
-let stage=undefined;
+
 
 const history = document.querySelector(".historySpan");
 const answer = document.querySelector(".answerSpan");
 
 const bAc = document.querySelector("#bAc");
 const bBs = document.querySelector("#bBs");
-const bD = document.querySelector("#bD");
-const bM = document.querySelector("#bM");
-const bS = document.querySelector("#bS");
-const bA = document.querySelector("#bA");
-const bE = document.querySelector("#bE");
 
+const boperations = document.querySelectorAll("button[target='operationButton']")
 const boper = document.querySelectorAll("#boper");
-    
+const bequal = document.querySelector("#bE");
+
 //BUTTONS
 boper.forEach((button) => {
      button.addEventListener("click",()=>{
@@ -30,6 +21,10 @@ boper.forEach((button) => {
 bAc.addEventListener("click", ()=>{
     answer.innerText=""; 
     history.innerText="";
+    data.operand1 = undefined; 
+    data.operand2 = undefined; 
+    data.operation = undefined; 
+    data.stage=0;
 });
 
 //BACKSPACE
@@ -37,3 +32,76 @@ bBs.addEventListener("click", ()=>{
     answer.innerText = answer.innerText.slice(0,-1); 
 });
 
+//LOGIC
+
+//VARIABLES
+let data = {
+    operand1: 32,
+    operation: '',
+    operand2: 31,
+    stage: 0
+};
+let oper = undefined;
+
+function operate(){
+    switch(data.operation){
+        case '+':   data.operand1 += data.operand2;
+                    break;
+        case '-':   data.operand1 -= data.operand2;
+                    break;
+        case '*':   data.operand1 *= data.operand2;
+                    break;
+        case '/':   data.operand1 /= data.operand2;
+                    break;
+    };
+    data.operand2 = undefined;
+}
+
+
+//OPERATION
+boperations.forEach((button)=>{
+    button.addEventListener("click",()=>{
+        if(answer.innerText != ""){
+            oper = button.innerText;
+            if(data.stage === 0){
+                data.stage = 1;
+                data.operand1 = parseInt(answer.innerText);
+                answer.innerText = "";
+                history.innerText = `${data.operand1} ${oper} `;  
+            }
+            else if(data.stage === 1 || data.stage === 2){
+                data.stage = 2; 
+                data.operand2 = parseInt(answer.innerText);
+                answer.innerText = "";
+                operate();
+                history.innerText = `${data.operand1} ${oper} `;        
+            }
+            data.operation = oper;
+        }
+        else{
+            oper = button.innerText;
+            data.operation = oper;
+            history.innerText = `${data.operand1} ${oper} `;  
+        }
+    })
+})
+
+bequal.addEventListener("click",()=>{
+        if(data.stage === 1 || data.stage === 2){
+            data.operand2 = parseInt(answer.innerText);
+            operate();
+            answer.innerText = `${data.operand1}`;
+            history.innerText = "";  
+        }
+        else if(data.stage === 0){
+            if(answer.innerText != "")
+                data.operand1 = parseInt(answer.innerText);
+            answer.innerText = `${data.operand1}`;
+            history.innerText = "";        
+        }
+
+    data.operand1 = undefined; 
+    data.operand2 = undefined; 
+    data.operation = undefined; 
+    data.stage=0;
+})
